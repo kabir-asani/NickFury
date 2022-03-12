a28434ed134457ee2e6a6e19ee04d173d14e54c9
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-03-12 20:39:11 +0530

    Add 'Network' (http request assistant)

---
+++ src/assistants/network/network.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+export class NetworkResult {
+    statusCode: Number;
+    data?: any;
+
+    constructor(parameters: {
+        statusCode: Number
+        data?: any,
+    }) {
+        this.statusCode = parameters.statusCode;
+        this.data = parameters.data;
+    }
+}
+
+export class Network {
+    private static _shared = new Network();
+    public static shared = (): Network => this._shared;
+
+    async get(parameters: {
+        url: String,
+        headers?: {
+            [key: string]: string
+        }
+    }): Promise<NetworkResult> {
+        const options = {
+            headers: parameters.headers,
+            validateStatus: () => true,
+        };
+
+        const result = await axios.get(
+            parameters.url.valueOf(),
+            options,
+        );
+
+        const reply = new NetworkResult({
+            statusCode: result.status,
+            data: result.data,
+        });
+
+        return reply;
+    }
+
+    async post(parameters: {
+        url: String,
+        headers?: {
+            [key: string]: string
+        },
+        body?: any
+    }): Promise<NetworkResult> {
+        const options = {
+            headers: parameters.headers,
+            data: parameters.body,
+            validateStatus: () => true,
+        }
+
+        const result = await axios.post(
+            parameters.url.valueOf(),
+            options,
+        );
+
+        const reply = new NetworkResult({
+            statusCode: result.status,
+            data: result.data,
+        });
+
+        return reply;
+    }
+}
