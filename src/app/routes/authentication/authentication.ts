2db3f413f7d222dead38c2bfc8320643caa5956c
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-03-17 21:56:12 +0530

    Add implementation for /logIn, /logOut routes

--- src/app/routes/authentication/authentication.ts
+++ src/app/routes/authentication/authentication.ts
@@ -1,25 +1,50 @@
 import { Request, Response, Router } from 'express';
 import Joi from 'joi';
-import { RouteFailure } from '../../core/types';
+import { AuthManager } from '../../../managers/authManager/authManager';
+import { authProvider, AuthProvider } from '../../../managers/authManager/models';
+import { LogInFailureReason } from '../../../managers/authManager/types';
+import { RouteFailure, RouteSuccess } from '../../core/types';
 import { gatekeeper } from '../../middlewares/gatekeeper/gatekeeper';
 import { GroundZero, soldier } from '../../middlewares/soldier/soldier';
 
 const authentication = Router();
 
-authentication.post('/logIn',
+authentication.post(
+    '/logIn',
     [
         soldier({
             schema: Joi.object({
                 accessToken: Joi.string().required(),
+                provider: Joi.string().allow([
+                    AuthProvider.apple,
+                    AuthProvider.google,
+                ])
             }),
             groundZero: GroundZero.body
         })
     ],
-    (req: Request, res: Response) => {
-        const failure = new RouteFailure('This route is currently under construction');
+    async (req: Request, res: Response) => {
+        const { accessToken, provider } = req.body;
 
-        res.status(501).json(failure);
-    });
+        const logInResult = await AuthManager.shared().logIn({
+            accessToken: accessToken as String,
+            provider: authProvider(provider)!,
+        });
+
+        logInResult.resolve({
+            onSuccess: (s) => {
+                const routeResult = new RouteSuccess(s);
+
+                res.status(201).json(routeResult);
+            },
+            onFailure: (f) => {
+                const routeResult = new RouteSuccess('Something went wrong');
+
+                res.status(501).json(routeResult);
+            }
+        });
+    }
+);
 
 
 authentication.post(
@@ -27,10 +52,25 @@ authentication.post(
     [
         ...gatekeeper()
     ],
-    (req: Request, res: Response) => {
-        const failure = new RouteFailure('This route is currently under construction');
+    async (req: Request, res: Response) => {
+        const { accessToken, provider } = req.body;
 
-        res.status(501).json(failure);
+        const logOutResult = await AuthManager.shared().logOut({
+            accessToken: accessToken as String,
+        });
+
+        logOutResult.resolve({
+            onSuccess: (s) => {
+                const routeResult = new RouteSuccess(s);
+
+                res.status(201).json(routeResult);
+            },
+            onFailure: (f) => {
+                const routeResult = new RouteSuccess('Something went wrong');
+
+                res.status(501).json(routeResult);
+            }
+        });
     }
 );
 
