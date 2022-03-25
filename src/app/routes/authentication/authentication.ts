2e3300be7fa4e8d2afc2f5489891b3c7f8c59d3f
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-03-25 21:31:10 +0530

    Refactor Authentication router

--- src/app/routes/authentication/authentication.ts
+++ src/app/routes/authentication/authentication.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 import Joi from 'joi';
 import { AuthManager } from '../../../managers/authManager/authManager';
 import { authProvider, AuthProvider } from '../../../managers/authManager/models';
-import { LogInFailureReason } from '../../../managers/authManager/types';
+import { IllegalAccessTokenFailure, LogInSuccess, LogOutSuccess } from '../../../managers/authManager/types';
 import { RouteFailure, RouteSuccess } from '../../core/types';
 import { gatekeeper } from '../../middlewares/gatekeeper/gatekeeper';
 import { GroundZero, soldier } from '../../middlewares/soldier/soldier';
@@ -15,10 +15,10 @@ authentication.post(
         soldier({
             schema: Joi.object({
                 accessToken: Joi.string().required(),
-                provider: Joi.string().allow([
-                    AuthProvider.apple,
-                    AuthProvider.google,
-                ])
+                provider: Joi.string().valid(
+                    AuthProvider.apple.valueOf(),
+                    AuthProvider.google.valueOf(),
+                ),
             }),
             groundZero: GroundZero.body
         })
@@ -26,23 +26,28 @@ authentication.post(
     async (req: Request, res: Response) => {
         const { accessToken, provider } = req.body;
 
-        const logInResult = await AuthManager.shared().logIn({
+        const logInResult = await AuthManager.shared.logIn({
             accessToken: accessToken as String,
             provider: authProvider(provider)!,
         });
 
-        logInResult.resolve({
-            onSuccess: (s) => {
-                const routeResult = new RouteSuccess(s);
+        if (logInResult instanceof LogInSuccess) {
+            const session = logInResult.session;
+            const result = new RouteSuccess(session);
 
-                res.status(201).json(routeResult);
-            },
-            onFailure: (f) => {
-                const routeResult = new RouteSuccess('Something went wrong');
+            res.status(201).json(result);
+            return;
+        }
 
-                res.status(501).json(routeResult);
-            }
-        });
+
+        if (logInResult instanceof IllegalAccessTokenFailure) {
+            const result = new RouteFailure('Incorrect access token');
+            res.status(400).json(result);
+            return;
+        }
+
+        const result = new RouteFailure('Something went wrong');
+        res.status(500).json(result);
     }
 );
 
@@ -53,24 +58,20 @@ authentication.post(
         ...gatekeeper()
     ],
     async (req: Request, res: Response) => {
-        const { accessToken, provider } = req.body;
+        const { accessToken } = req.body;
 
-        const logOutResult = await AuthManager.shared().logOut({
+        const logOutResult = await AuthManager.shared.logOut({
             accessToken: accessToken as String,
         });
 
-        logOutResult.resolve({
-            onSuccess: (s) => {
-                const routeResult = new RouteSuccess(s);
+        if (logOutResult instanceof LogOutSuccess) {
+            const result = new RouteSuccess('Successfully logged out');
+            res.status(200).json(result);
+            return;
+        }
 
-                res.status(201).json(routeResult);
-            },
-            onFailure: (f) => {
-                const routeResult = new RouteSuccess('Something went wrong');
-
-                res.status(501).json(routeResult);
-            }
-        });
+        const result = new RouteFailure('Something went wrong');
+        res.status(500).json(result);
     }
 );
 
