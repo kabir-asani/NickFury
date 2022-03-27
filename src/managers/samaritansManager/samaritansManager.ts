4aacb4bcfaadc60d9578113a59951ca17c09ee34
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-03-27 18:30:41 +0530

    Add exists method to SamaritansManager

--- src/managers/samaritansManager/samaritansManager.ts
+++ src/managers/samaritansManager/samaritansManager.ts
@@ -9,20 +9,81 @@ import { SamaritanAlreadyExists, CreateSamaritanFailure, CreateSamaritanSuccess 
 export class SamaritansManager {
     public static readonly shared = new SamaritansManager();
 
-    async createSamaritan(details: {
+    async exists(parameters: {
+        sid?: String,
+        username?: String,
+        email?: String,
+    }): Promise<Boolean> {
+        assert(
+            parameters.sid !== undefined || parameters.username !== undefined || parameters.email !== undefined,
+            "One of id, username or email has to be present"
+        );
+
+        if (parameters.sid !== undefined) {
+            const collectionRef = DatabaseAssistant.shared.collection(TxDatabaseCollections.samaritans);
+            const documentRef = collectionRef.doc(parameters.sid.valueOf());
+
+            const samaritan = await documentRef.get();
+
+            if (samaritan.exists) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (parameters.username !== undefined) {
+            const collectionRef = DatabaseAssistant.shared.collection(TxDatabaseCollections.samaritans);
+            const query = collectionRef.where(
+                "username",
+                "==",
+                parameters.username.valueOf(),
+            );
+
+            const querySnapshot = await query.get();
+
+            if (querySnapshot.empty) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (parameters.email !== undefined) {
+            const collectionRef = DatabaseAssistant.shared.collection(TxDatabaseCollections.samaritans);
+            const query = collectionRef.where(
+                "email",
+                "==",
+                parameters.email.valueOf(),
+            );
+
+            const querySnapshot = await query.get();
+
+            if (querySnapshot.empty) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        
+        return false;
+    }
+
+    async createSamaritan(parameters: {
         name: String;
         email: String;
         image: String;
     }): Promise<CreateSamaritanSuccess | CreateSamaritanFailure> {
         const sid = uuid.v4();
-        const username = details.email.split('@')[0] + uuid.v4().substring(0, 5);
+        const username = parameters.email.split('@')[0] + uuid.v4().substring(0, 5);
 
         const samaritan: Samaritan = {
             sid,
-            name: details.name,
-            email: details.email,
+            name: parameters.name,
+            email: parameters.email,
             username,
-            image: details.image,
+            image: parameters.image,
             creationDate: Date.now(),
             socialDetails: {
                 followersCount: 0,
