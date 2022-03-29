e0b4f39de1bd99586863f5a905c2f926e4e5c219
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-03-30 01:48:29 +0530

    Add feed route to samaritans' router

--- src/app/routes/samaritans/samaritans.ts
+++ src/app/routes/samaritans/samaritans.ts
@@ -2,10 +2,11 @@ import { Request, Response, Router } from 'express';
 import Joi from 'joi';
 import { SamaritansManager } from '../../../managers/samaritansManager/samaritansManager';
 import { SessionsManager } from '../../../managers/sessionManager/sessionsManager';
+import { TweetsManager } from '../../../managers/tweetsManager/tweetsManager';
 import { MissingResourceRouteFailure, InternalRouteFailure } from '../../core/types';
 import { gatekeeper } from '../../middlewares/gatekeeper/gatekeeper';
 import { GroundZero, soldier } from '../../middlewares/soldier/soldier';
-import { SamaritanRouteSuccess } from './types';
+import { FeedRouteSuccess, SamaritanRouteSuccess } from './types';
 
 const samaritans = Router();
 
@@ -75,6 +76,71 @@ samaritans.get(
 
             res
                 .status(MissingResourceRouteFailure.statusCode)
+                .json(response);
+
+            return;
+        }
+
+        const response = new SamaritanRouteSuccess(samaritan);
+
+        res
+            .status(SamaritanRouteSuccess.statusCode)
+            .json(response);
+    }
+);
+
+samaritans.get(
+    '/:sid/feed',
+    [
+        ...gatekeeper(),
+        soldier({
+            schema: Joi.object({
+                sid: Joi.string().required(),
+            }),
+            groundZero: GroundZero.parameters
+        }),
+        soldier({
+            schema: Joi.object({
+                nextToken: Joi.string(),
+                limit: Joi.number().max(100),
+            }),
+            groundZero: GroundZero.query
+        })
+    ],
+    async (req: Request, res: Response) => {
+        const sid = req.params.sid as String;
+        const limit = req.query.limit;
+        const nextToken = req.query.nextToken;
+
+        const samaritan = await SamaritansManager.shared.samaritan({
+            sid: sid
+        });
+
+        if (samaritan === null) {
+            const response = new MissingResourceRouteFailure();
+
+            res
+                .status(MissingResourceRouteFailure.statusCode)
+                .json(response);
+
+            return;
+        }
+
+        const feed = await TweetsManager.shared.feed({
+            sid: sid,
+            limit: limit !== undefined || limit !== null
+                ? limit as unknown as Number
+                : undefined,
+            nextToken: nextToken !== undefined || nextToken !== null
+                ? nextToken as unknown as String
+                : undefined,
+        });
+
+        if (feed !== null) {
+            const response = new FeedRouteSuccess(feed);
+
+            res
+                .status(FeedRouteSuccess.statusCode)
                 .json(response);
 
             return;
--- src/app/routes/samaritans/types.ts
+++ src/app/routes/samaritans/types.ts
@@ -1,4 +1,5 @@
 import { Samaritan } from "../../../managers/samaritansManager/models";
+import { Feed } from "../../../managers/tweetsManager/types";
 import { RouteSuccess } from "../../core/types";
 
 export class SamaritanRouteSuccess extends RouteSuccess {
@@ -6,5 +7,13 @@ export class SamaritanRouteSuccess extends RouteSuccess {
 
     constructor(samaritan: Samaritan) {
         super(samaritan);
+    }
+}
+
+export class FeedRouteSuccess extends RouteSuccess {
+    static readonly statusCode: 200;
+
+    constructor(feed: Feed) {
+        super(feed);
     }
 }
