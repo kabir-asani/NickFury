89f03c00bd574aa896411a6c71374d5f4ca995ac
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-03-31 17:22:29 +0530

    Add return type to paginated middleware

--- src/app/middlewares/paginated/paginated.ts
+++ src/app/middlewares/paginated/paginated.ts
@@ -1,7 +1,8 @@
 import Joi from "joi";
+import { TxMiddleware } from "../core/types";
 import { soldier, GroundZero } from "../soldier/soldier";
 
-const paginated = () => soldier({
+const paginated = (): TxMiddleware => soldier({
     schema: Joi.object({
         limit: Joi.number().max(100),
         nextToken: Joi.string(),
