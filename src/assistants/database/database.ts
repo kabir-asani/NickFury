3b32aec47a0217bd852ca9ae18bb4c117ee98495
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-15 01:16:33 +0530

    Refactor collections.ts into database.ts

--- src/assistants/database/database.ts
+++ src/assistants/database/database.ts
@@ -1,6 +1,19 @@
 import { firestore } from "firebase-admin";
 import { FirebaseAssistant } from "../firebase/firebase";
 
+export class DatabaseCollections {
+    static readonly sessions = "sessions";
+
+    static readonly tweets = "tweets";
+    static readonly likes = "likes";
+    static readonly comments = "comments";
+
+    static readonly users = "users";
+    static readonly followings = "followings";
+    static readonly followers = "followers";
+    static readonly bookmarks = "bookmarks";
+}
+
 export class DatabaseAssistant {
     public static readonly shared = firestore(FirebaseAssistant.shared().app());
 
