85cac4463046322b952dff552915737668c01cd7
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-16 09:48:12 +0530

    Partially implement "/users/:userId" route

--- src/app/routes/users/others.ts
+++ src/app/routes/users/others.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
-import { UnimplementedRouteFailure } from "../../core/types";
+import { UsersManager } from "../../../managers/usersManager/usersManager";
+import { SessionizedRequest } from "../../core/override";
+import { AllOkRouteSuccess, UnimplementedRouteFailure } from "../../core/types";
 import followers from "./socials/followers/followers";
 import followings from "./socials/followings/followings";
 import tweets from "./tweets/tweets";
@@ -17,11 +19,30 @@ others.use("/tweets", tweets);
 others.get(
     "/",
     async (req: Request, res: Response) => {
-        const response = new UnimplementedRouteFailure();
+        const session = (req as SessionizedRequest).session;
 
-        res
-            .status(UnimplementedRouteFailure.statusCode)
-            .json(response);
+        const userId = req.params.userId as String;
+
+
+        const user = await UsersManager.shared.user({
+            id: userId
+        });
+
+        if (user !== null) {
+            // TODO: Fetch and insert viewables
+
+            const response = new AllOkRouteSuccess(user);
+
+            res
+                .status(AllOkRouteSuccess.statusCode)
+                .json(response);
+        } else {
+            const response = new UnimplementedRouteFailure();
+
+            res
+                .status(UnimplementedRouteFailure.statusCode)
+                .json(response);
+        }
     }
 );
 
