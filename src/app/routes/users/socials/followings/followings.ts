ae583e18683adc2941af8a9fd9a6ee4997f0b487
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-16 11:18:53 +0530

    Implement follow and unfollow routes

--- src/app/routes/users/socials/followings/followings.ts
+++ src/app/routes/users/socials/followings/followings.ts
@@ -1,7 +1,20 @@
 import { Router, Request, Response } from "express";
 import Joi from "joi";
-import { NoResourceRouteFailure, UnimplementedRouteFailure } from "../../../../core/types";
+import { SocialsManager } from "../../../../../managers/socialsManager/socialsManager";
+import {
+    FollowFailureReason,
+    UnfollowFailureReason
+} from "../../../../../managers/socialsManager/types";
+import { Failure } from "../../../../../utils/typescriptx/typescriptx";
+import { SessionizedRequest } from "../../../../core/override";
+import {
+    InternalRouteFailure,
+    NoContentRouteSuccess,
+    NoResourceRouteFailure,
+    UnimplementedRouteFailure
+} from "../../../../core/types";
 import paginated from "../../../../middlewares/paginated/paginated";
+import { selfishGuard } from "../../../../middlewares/selfieGuard/selfieGuard";
 import { GroundZero, soldier } from "../../../../middlewares/soldier/soldier";
 
 const followings = Router({
@@ -22,35 +35,107 @@ followings.get(
 
 followings.post(
     "/",
-    soldier({
-        schema: Joi.object({
-            userId: Joi.string().required(),
-        }),
-        groundZero: GroundZero.body,
-    }),
+    [
+        selfishGuard(),
+        soldier({
+            schema: Joi.object({
+                userId: Joi.string().required(),
+            }),
+            groundZero: GroundZero.body,
+        })
+    ],
     async (req: Request, res: Response) => {
-        const response = new UnimplementedRouteFailure();
+        const session = (req as SessionizedRequest).session;
 
-        res
-            .status(UnimplementedRouteFailure.statusCode)
-            .json(response);
+        const parameters = req.body as {
+            userId: String
+        };
+
+        const followResult = await SocialsManager.shared.follow({
+            followerId: session.userId,
+            followingId: parameters.userId
+        });
+
+        if (followResult instanceof Failure) {
+            switch (followResult.reason) {
+                case FollowFailureReason.followingDoesNotExists: {
+                    const response = new NoResourceRouteFailure();
+
+                    res
+                        .status(NoResourceRouteFailure.statusCode)
+                        .json(response);
+
+                    break;
+                }
+                default: {
+                    const response = new InternalRouteFailure();
+
+                    res
+                        .status(InternalRouteFailure.statusCode)
+                        .json(response);
+
+                    break;
+                }
+            }
+        } else {
+            const response = new NoContentRouteSuccess();
+
+            res
+                .status(NoContentRouteSuccess.statusCode)
+                .json(response);
+        }
     },
 );
 
 followings.delete(
     "/:userId",
-    soldier({
-        schema: Joi.object({
-            userId: Joi.string().required(),
-        }),
-        groundZero: GroundZero.parameters,
-    }),
+    [
+        selfishGuard(),
+        soldier({
+            schema: Joi.object({
+                userId: Joi.string().required(),
+            }),
+            groundZero: GroundZero.parameters,
+        })
+    ],
     async (req: Request, res: Response) => {
-        const response = new UnimplementedRouteFailure();
+        const session = (req as SessionizedRequest).session;
 
-        res
-            .status(UnimplementedRouteFailure.statusCode)
-            .json(response);
+        const userId = req.params.userId as String;
+
+        const unfollowResult = await SocialsManager.shared.unfollow({
+            followerId: session.userId,
+            followingId: userId
+        });
+
+        if (unfollowResult instanceof Failure) {
+            switch (unfollowResult.reason) {
+                case UnfollowFailureReason.relationshipDoesNotExists: {
+                    const response = new NoResourceRouteFailure();
+
+                    res
+                        .status(NoResourceRouteFailure.statusCode)
+                        .json(response);
+
+                    break;
+                }
+                default: {
+                    const response = new InternalRouteFailure();
+
+                    res
+                        .status(InternalRouteFailure.statusCode)
+                        .json(response);
+
+                    break;
+                }
+            }
+        } else {
+            const response = new NoContentRouteSuccess();
+
+            res
+                .status(NoContentRouteSuccess.statusCode)
+                .json(response);
+        }
     }
 )
 
