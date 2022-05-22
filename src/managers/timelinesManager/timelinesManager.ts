206ab2311a48a435ad6dc17f5bd40eb433962e00
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-22 18:03:12 +0530

    Implement timeline method on TimelinesManager

--- src/managers/timelinesManager/timelinesManager.ts
+++ src/managers/timelinesManager/timelinesManager.ts
@@ -1,6 +1,7 @@
 import { StreamAssistant } from "../../assistants/stream/stream";
-import { Tweet } from "../core/models";
+import { Tweet, ViewableTweet } from "../core/models";
 import { Paginated, PaginationParameters } from "../core/types";
+import { TweetsManager } from "../tweetsManager/tweetsManager";
 
 export class TimelinesManager {
     static readonly shared = new TimelinesManager();
@@ -9,14 +10,36 @@ export class TimelinesManager {
 
     async timeline(parameters: {
         userId: String
-    } & PaginationParameters): Promise<Paginated<Tweet> | null> {
-        // TODO: Implement `TimelinesManager.timeline`
-
+    } & PaginationParameters): Promise<Paginated<ViewableTweet> | null> {
         const activities = await StreamAssistant.shared.timelineFeed.activities({
             userId: parameters.userId,
             nextToken: parameters.nextToken,
             limit: parameters.limit,
         });
+
+        if (activities !== null) {
+            let tweets: ViewableTweet[] = [];
+
+            for (let activity of activities.page) {
+                const tweet = await TweetsManager.shared.tweet({
+                    tweetId: activity.tweetId,
+                    viewerId: parameters.userId
+                });
+
+                if (tweet !== null) {
+                    tweets.push(tweet as ViewableTweet);
+                } else {
+                    return null;
+                }
+            }
+
+            const reply: Paginated<ViewableTweet> = {
+                page: tweets,
+                nextToken: activities.nextToken
+            };
+
+            return reply;
+        }
 
         return null;
     }
