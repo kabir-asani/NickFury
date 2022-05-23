b4ec63531b1f2830b220cce035ff55f1eb2f4f1a
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-23 14:05:07 +0530

    Fix issues on paginated middleware

--- src/app/middlewares/paginated/paginated.ts
+++ src/app/middlewares/paginated/paginated.ts
@@ -5,8 +5,8 @@ import { soldier, GroundZero } from "../soldier/soldier";
 
 const paginated = (): TxMiddleware => soldier({
     schema: Joi.object({
-        limit: Joi.number().max(kMaximumPaginatedPageLength),
-        nextToken: Joi.string(),
+        limit: Joi.number().max(kMaximumPaginatedPageLength).optional(),
+        nextToken: Joi.string().optional(),
     }).unknown(true),
     groundZero: GroundZero.query,
 });
