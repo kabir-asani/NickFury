5cc849e4dfc968608d5f243764103f3a67f94b16
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-25 00:39:24 +0530

    Implement timeline routes

--- src/app/routes/users/timeline/timeline.ts
+++ src/app/routes/users/timeline/timeline.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from "express";
-import { UnimplementedRouteFailure } from "../../../core/types";
+import { kMaximumPaginatedPageLength } from "../../../../managers/core/types";
+import { TimelinesManager } from "../../../../managers/timelinesManager/timelinesManager";
+import { SessionizedRequest } from "../../../core/override";
+import { AllOkRouteSuccess, InternalRouteFailure, UnimplementedRouteFailure } from "../../../core/types";
 import paginated from "../../../middlewares/paginated/paginated";
+import { selfishGuard } from "../../../middlewares/selfieGuard/selfieGuard";
 
 const timeline = Router({
     mergeParams: true
@@ -8,13 +12,37 @@ const timeline = Router({
 
 timeline.get(
     "/",
-    paginated(),
+    [
+        selfishGuard(),
+        paginated(),
+    ],
     async (req: Request, res: Response) => {
-        const response = new UnimplementedRouteFailure();
+        const session = (req as SessionizedRequest).session;
 
-        res
-            .status(UnimplementedRouteFailure.statusCode)
-            .json(response);
+        const nextToken = req.params.nextToken;
+        const limit = parseInt(req.params.limit);
+
+        const safeLimit = isNaN(limit) ? kMaximumPaginatedPageLength : limit;
+
+        const timeline = await TimelinesManager.shared.timeline({
+            userId: session.userId,
+            nextToken: nextToken,
+            limit: safeLimit
+        });
+
+        if (timeline !== null) {
+            const response = new AllOkRouteSuccess(timeline);
+
+            res
+                .status(AllOkRouteSuccess.statusCode)
+                .json(response);
+        } else {
+            const response = new InternalRouteFailure();
+
+            res
+                .status(InternalRouteFailure.statusCode)
+                .json(response);
+        }
     },
 );
 
