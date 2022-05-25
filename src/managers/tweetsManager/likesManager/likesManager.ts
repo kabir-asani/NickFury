a9f6644039c3e8ac4f5270317cb723348d02ee81
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-25 22:59:02 +0530

    Impelement LikesManager.exists method

--- src/managers/tweetsManager/likesManager/likesManager.ts
+++ src/managers/tweetsManager/likesManager/likesManager.ts
@@ -1,3 +1,5 @@
+import { assert } from "console";
+import { DatabaseAssistant, DatabaseCollections } from "../../../assistants/database/database";
 import { Empty, Failure, Success } from "../../../utils/typescriptx/typescriptx";
 import { Like, ViewableLike } from "../../core/models";
 import { Paginated, PaginationParameters, ViewablesParameters } from "../../core/types";
@@ -15,8 +17,49 @@ export class LikesManager {
             authorId: String;
         }
     }): Promise<Boolean> {
-        // TODO: Implement `LikesManager.exists`
-        return false;
+        assert(
+            parameters.likeId !== undefined || parameters.likeDetails !== undefined,
+            "At least one of likeId or likeDetails should be present"
+        );
+
+        const likesCollection = DatabaseAssistant.shared.collectionGroup(DatabaseCollections.likes);
+
+        let likesQuery = likesCollection
+            .limit(1);
+
+        if (parameters.likeId !== undefined) {
+            likesQuery = likesQuery
+                .where(
+                    "id",
+                    "==",
+                    parameters.likeId.valueOf()
+                );
+        } else if (parameters.likeDetails !== undefined) {
+            likesQuery = likesQuery
+                .where(
+                    "tweetId",
+                    "==",
+                    parameters.likeDetails.tweetId.valueOf()
+                )
+                .where(
+                    "authorId",
+                    "==",
+                    parameters.likeDetails.authorId
+                );
+        }
+
+
+        try {
+            const likesQuerySnapshot = await likesQuery.get();
+
+            if (likesQuerySnapshot.docs.length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch {
+            return false;
+        }
     }
 
     async create(parameters: {
