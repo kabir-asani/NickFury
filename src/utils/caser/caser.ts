fe7346bab9c8a9269fdd832f9127e29474c5507c
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-27 15:57:18 +0530

    Add senteceCasize util to caser.ts

--- src/utils/caser/caser.ts
+++ src/utils/caser/caser.ts
@@ -7,3 +7,21 @@ export const camelCasize = (data: Object): Object => {
 export const snakeCasize = (data: Object): Object => {
     return humps.decamelizeKeys(data, { separator: "_" });
 };
+
+export const sentenceCasize = (data: String): String => {
+    if (data.length === 0) {
+        return data;
+    }
+
+    if (data.length === 1) {
+        return data.charAt(0).toUpperCase();
+    }
+
+    const auxResult = data
+        .replace(/([A-Z]+)/g, " $1")
+        .replace(/([A-Z][a-z])/g, " $1");
+
+    const result = auxResult.charAt(0).toUpperCase() + auxResult.slice(1);
+
+    return result;
+};
