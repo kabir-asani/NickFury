110a3950b366303f495eec343522abe89be45962
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-05-27 23:16:02 +0530

    Implement likes and coments routers

--- src/app/routes/tweets/reactions/comments.ts
+++ src/app/routes/tweets/reactions/comments.ts
@@ -1,6 +1,20 @@
 import { Router, Request, Response } from "express";
 import Joi from "joi";
-import { UnimplementedRouteFailure } from "../../../core/types";
+import CommentsManager from "../../../../managers/tweetsManager/commentsManager/commentsManager";
+import {
+    CommentDeletionFailureReason,
+    PaginatedViewableCommentsFailureReason,
+} from "../../../../managers/tweetsManager/commentsManager/types";
+import { sentenceCasize } from "../../../../utils/caser/caser";
+import { Failure } from "../../../../utils/typescriptx/typescriptx";
+import { SessionizedRequest } from "../../../core/override";
+import {
+    AllOkRouteSuccess,
+    InternalRouteFailure,
+    NoContentRouteSuccess,
+    NoResourceRouteFailure,
+    SemanticRouteFailure,
+} from "../../../core/types";
 import paginated from "../../../middlewares/paginated/paginated";
 import soldier, { GroundZero } from "../../../middlewares/soldier/soldier";
 
@@ -9,9 +23,48 @@ const comments = Router({
 });
 
 comments.get("/", paginated(), async (req: Request, res: Response) => {
-    const response = new UnimplementedRouteFailure();
+    type NewType = SessionizedRequest;
 
-    res.status(UnimplementedRouteFailure.statusCode).json(response);
+    const session = (req as NewType).session;
+
+    const tweetId = req.params.tweetId;
+
+    const paginatedCommentsResult =
+        await CommentsManager.shared.paginatedViewableCommentsOf({
+            tweetId: tweetId,
+            viewerId: session.userId,
+        });
+
+    if (paginatedCommentsResult instanceof Failure) {
+        const message = sentenceCasize(
+            PaginatedViewableCommentsFailureReason[
+                paginatedCommentsResult.reason
+            ]
+        );
+
+        switch (paginatedCommentsResult.reason) {
+            case PaginatedViewableCommentsFailureReason.malformedParameters: {
+                const response = new SemanticRouteFailure(message);
+
+                res.status(SemanticRouteFailure.statusCode).json(response);
+
+                return;
+            }
+            default: {
+                const response = new InternalRouteFailure();
+
+                res.status(InternalRouteFailure.statusCode).json(response);
+
+                return;
+            }
+        }
+    }
+
+    const paginatedComments = paginatedCommentsResult.data;
+
+    const response = new AllOkRouteSuccess(paginatedComments);
+
+    res.status(AllOkRouteSuccess.statusCode).json(response);
 });
 
 comments.post(
@@ -23,10 +76,69 @@ comments.post(
         groundZero: GroundZero.body,
     }),
     async (req: Request, res: Response) => {
-        const response = new UnimplementedRouteFailure();
+        const session = (req as SessionizedRequest).session;
 
-        res.status(UnimplementedRouteFailure.statusCode).json(response);
+        const tweetId = req.params.tweetId;
+
+        const parameters = req.body as {
+            text: String;
+        };
+
+        const commentCreationResult = await CommentsManager.shared.create({
+            tweetId: tweetId,
+            authorId: session.userId,
+            comment: {
+                text: parameters.text,
+            },
+        });
+
+        if (commentCreationResult instanceof Failure) {
+            const response = new InternalRouteFailure();
+
+            res.status(InternalRouteFailure.statusCode).json(response);
+
+            return;
+        }
+
+        const response = new NoContentRouteSuccess();
+
+        res.status(NoContentRouteSuccess.statusCode).json(response);
     }
 );
+
+comments.delete("/:commentId", async (req: Request, res: Response) => {
+    const commentId = req.params.commentId;
+
+    const commentDeletionResult = await CommentsManager.shared.delete({
+        commentId: commentId,
+    });
+
+    if (commentDeletionResult instanceof Failure) {
+        const message = sentenceCasize(
+            CommentDeletionFailureReason[commentDeletionResult.reason]
+        );
+
+        switch (commentDeletionResult.reason) {
+            case CommentDeletionFailureReason.commentDoesNotExists: {
+                const response = new NoResourceRouteFailure(message);
+
+                res.status(NoResourceRouteFailure.statusCode).json(response);
+
+                return;
+            }
+            default: {
+                const response = new InternalRouteFailure(message);
+
+                res.status(InternalRouteFailure.statusCode).json(response);
+
+                return;
+            }
+        }
+    }
+
+    const response = new NoContentRouteSuccess();
+
+    res.status(NoContentRouteSuccess.statusCode).json(response);
+});
 
 export default comments;
--- src/app/routes/tweets/reactions/likes.ts
+++ src/app/routes/tweets/reactions/likes.ts
@@ -1,5 +1,21 @@
 import { Router, Request, Response } from "express";
-import { UnimplementedRouteFailure } from "../../../core/types";
+import LikesManager from "../../../../managers/tweetsManager/likesManager/likesManager";
+import {
+    LikeCreationFailureReason,
+    LikeDeletionFailureReason,
+    PaginatedViewableLikesFailureReason,
+} from "../../../../managers/tweetsManager/likesManager/types";
+import { sentenceCasize } from "../../../../utils/caser/caser";
+import { Failure } from "../../../../utils/typescriptx/typescriptx";
+import { SessionizedRequest } from "../../../core/override";
+import {
+    AllOkRouteSuccess,
+    ConflictRouteFailure,
+    InternalRouteFailure,
+    NoContentRouteSuccess,
+    NoResourceRouteFailure,
+    SemanticRouteFailure,
+} from "../../../core/types";
 import paginated from "../../../middlewares/paginated/paginated";
 
 const likes = Router({
@@ -7,21 +23,117 @@ const likes = Router({
 });
 
 likes.get("/", paginated(), async (req: Request, res: Response) => {
-    const response = new UnimplementedRouteFailure();
+    const session = (req as SessionizedRequest).session;
 
-    res.status(UnimplementedRouteFailure.statusCode).json(response);
+    const tweetId = req.params.tweetId;
+
+    const paginatedLikesResult =
+        await LikesManager.shared.paginatedViewableLikesOf({
+            tweetId: tweetId,
+            viewerId: session.userId,
+        });
+
+    if (paginatedLikesResult instanceof Failure) {
+        const message = sentenceCasize(
+            PaginatedViewableLikesFailureReason[paginatedLikesResult.reason]
+        );
+
+        switch (paginatedLikesResult.reason) {
+            case PaginatedViewableLikesFailureReason.malformedParameters: {
+                const response = new SemanticRouteFailure(message);
+
+                res.status(SemanticRouteFailure.statusCode).json(response);
+
+                return;
+            }
+            default: {
+                const response = new InternalRouteFailure(message);
+
+                res.status(InternalRouteFailure.statusCode).json(response);
+
+                return;
+            }
+        }
+    }
+
+    const paginatedLikes = paginatedLikesResult.data;
+
+    const response = new AllOkRouteSuccess(paginatedLikes);
+
+    res.status(AllOkRouteSuccess.statusCode).json(response);
 });
 
 likes.post("/", async (req: Request, res: Response) => {
-    const response = new UnimplementedRouteFailure();
+    const session = (req as SessionizedRequest).session;
 
-    res.status(UnimplementedRouteFailure.statusCode).json(response);
+    const tweetId = req.params.tweetId;
+
+    const likeCreationResult = await LikesManager.shared.create({
+        tweetId: tweetId,
+        authorId: session.userId,
+    });
+
+    if (likeCreationResult instanceof Failure) {
+        const message = sentenceCasize(
+            LikeCreationFailureReason[likeCreationResult.reason]
+        );
+
+        switch (likeCreationResult.reason) {
+            case LikeCreationFailureReason.likeAlreadyExists: {
+                const response = new ConflictRouteFailure(message);
+
+                res.status(ConflictRouteFailure.statusCode).json(response);
+
+                return;
+            }
+            default: {
+                const response = new InternalRouteFailure(message);
+
+                res.status(InternalRouteFailure.statusCode).json(response);
+
+                return;
+            }
+        }
+    }
+
+    const response = new NoContentRouteSuccess();
+
+    res.status(NoContentRouteSuccess.statusCode).json(response);
 });
 
 likes.delete("/:likeId", async (req: Request, res: Response) => {
-    const response = new UnimplementedRouteFailure();
+    const likeId = req.params.likeId;
 
-    res.status(UnimplementedRouteFailure.statusCode).json(response);
+    const likeDeletionResult = await LikesManager.shared.delete({
+        likeId: likeId,
+    });
+
+    if (likeDeletionResult instanceof Failure) {
+        const message = sentenceCasize(
+            LikeDeletionFailureReason[likeDeletionResult.reason]
+        );
+
+        switch (likeDeletionResult.reason) {
+            case LikeDeletionFailureReason.likeDoesNotExists: {
+                const response = new NoResourceRouteFailure(message);
+
+                res.status(NoResourceRouteFailure.statusCode).json(response);
+
+                return;
+            }
+            default: {
+                const response = new InternalRouteFailure(message);
+
+                res.status(InternalRouteFailure.statusCode).json(response);
+
+                return;
+            }
+        }
+    }
+
+    const response = new NoContentRouteSuccess();
+
+    res.status(NoContentRouteSuccess.statusCode).json(response);
 });
 
 export default likes;
